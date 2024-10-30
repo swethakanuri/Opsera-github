78793143d46f0b780ad609acd646bd1b74655a60
swethakanuri <61013408+swethakanuri@users.noreply.github.com>
2024-10-30 16:12:21 +0530

    Create opsera.java

---
+++ opsera.java
@@ -0,0 +1,12 @@
+public class BasicMath {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = addNumbers(num1, num2);
+        System.out.println("The sum is: " + sum);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
